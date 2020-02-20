83606299fbb4e50d4ee3ef44f66e5c558445c9fa
surajv1986 <surajv1986@gmail.com>
2020-02-20 21:55:30 +0530

    2nd commit: partially written excersise1-9
    Signed-off-by: surajv1986 <surajv1986@gmail.com>

---
+++ excersise1-9.c
@@ -0,0 +1,30 @@
+/* Program to copy input to string to output and replace multiple spaces by a single space */
+#include<stdio.h>
+#include<stdbool.h>
+
+#define SIZE 100
+
+int main()
+{
+	char ip[SIZE];
+	char op[SIZE];
+	int index n1 = 0, n2 = 0;
+	int n = 0;
+	bool inWord = true;
+
+	
+	/* Accept input string upto newline*/
+	while (ip = getchar() != '\n') {
+		n++;
+	}
+	ip[n] = '\0';
+	while(i < n) {
+	
+		if(ip[i] == " ") {
+			
+		}
+	
+	}		
+
+
+}
