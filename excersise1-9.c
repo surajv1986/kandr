9e92c893ed3df611208df95bd54665b5cc74b1b8
surajv1986 <surajv1986@gmail.com>
2020-04-13 02:48:54 +0530

    Backup Commit:Incorrect logic
    Signed-off-by: surajv1986 <surajv1986@gmail.com>

--- excersise1-9.c
+++ excersise1-9.c
@@ -1,30 +1,56 @@
 /* Program to copy input to string to output and replace multiple spaces by a single space */
 #include<stdio.h>
+#include<stdlib.h>
 #include<stdbool.h>
+#include<string.h>
 
 #define SIZE 100
 
 int main()
 {
 	char ip[SIZE];
-	char op[SIZE];
-	int index n1 = 0, n2 = 0;
+	char op[SIZE], c;
+	int i = 0, j = 0, k = 0, n1 = 0, n2 = 0;
 	int n = 0;
-	bool inWord = true;
+	bool inWord = true, hasMSpace = false;
 
-	
+	memset(ip, 0, sizeof(ip));
+	memset(op, 0, sizeof(op));
+	printf("Enter a string\n");
 	/* Accept input string upto newline*/
-	while (ip = getchar() != '\n') {
+	while ((c = getchar()) != '\n') {
+		ip[i++] = c;
 		n++;
 	}
-	ip[n] = '\0';
-	while(i < n) {
-	
-		if(ip[i] == " ") {
-			
-		}
-	
-	}		
-
-
+	//ip[n] = '\0';
+	printf("The string entered is\n");
+	puts(ip);
+	i = 0;
+	while (i <= n) {
+		/* if space is found */
+		if (ip[i] == ' ') {
+			/* If there is only a single space*/
+			if( k == 1) {
+				/* copy the single space to the output string */
+				op[j] = ' ';
+				/* increment output buffer index */
+				j++;
+			}
+			/* count the number of spaces*/
+			k++;
+			if ( k > 1) {
+				i++;
+			}
+			continue;				
+		} else if ( k > 1) { /* If the number of spaces is more than one */		
+			op[j] = ip[i - 1];
+		} else {		
+			op[j] = ip[i];
+		}	      	
+		/* Increment loop variables */
+		i++;
+		j++;
+	}
+	puts("The truncated string is \n");
+	puts(op);	
 }
