315c1a926da061c5c2ac2146c8d8f12daf72748c
surajv1986 <surajv1986@gmail.com>
2020-04-15 15:38:00 +0530

    Successful working program cleaned using checkpatch:
    Signed-off-by: surajv1986 <surajv1986@gmail.com>

---
+++ ex1.13.c
@@ -0,0 +1,65 @@
+/* A C Program to print the histogram of the length of words in its input */
+#include<stdio.h>
+#include<string.h>
+
+#define SIZE 1000
+/* Function prototypes */
+int draw_histogram(int *count, int n);
+
+
+int main(void)
+{
+	char ip[SIZE], c;
+	/* loop variables */
+	int i = 0, j = 0, count[SIZE] = {0};
+	/* index variables */
+	int n = 0, k = 1;
+
+	puts("Enter an arbitary string terminated by a new line");
+	while ((c = getchar()) != '\n') {
+		ip[i++] = c;
+		n++;
+	}
+	ip[n] = '\0';
+	puts(ip);
+	/* reset loop variable */
+	i = 0;
+	/* For each element in string */
+	while (i < n) {
+		if (ip[i] == ' ') {
+			j++;
+			k++;
+		} else
+			count[j]++;
+		i++;
+
+	}
+	for (j = 0; j < k; j++) {
+		printf("%d\n", count[j]);
+	}
+	draw_histogram(count, k);
+
+	return 0;
+}
+
+int draw_histogram(int *count, int n)
+{
+	int i = 0, j = 0;
+
+	puts("The required horizontal histogram is");
+	/* loop through the histogram array */
+	while (i <  n) {
+		/* reset loop variable j */
+		j = 0;
+		/* draw '*' until count */
+		while (j < *count) {
+			printf("*");
+			j++;
+		}
+		printf("\n");
+		i++;
+		count++;
+	}
+
+	return 0;
+}
